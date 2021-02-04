ea2fa0c30e37b0c639ae0a4edf3c5a3e2e027fd3
Alexey Ruzin <ruzin@kokoc.com>
2021-02-04 21:49:12 +0300

    __add__

--- lesson_7/classwork.py
+++ lesson_7/classwork.py
@@ -1,51 +1,22 @@
 
-def prepend_dashes(func):
-    def xxx(*args, **kwargs):
-        print('----')
-        result = func(*args, **kwargs)
-        print('====')
-        return result
-    return xxx
+class A:
+    def __init__(self, a):
+        self.a = a
 
+    def __str__(self):
+        return f'!{self.a}!'
 
-@prepend_dashes
-def move_to(place):
-    print('move_to', place)
+    def __add__(self, other):
+        if hasattr(other, 'a'):
+            return A(self.a + other.a)
+        else:
+            return A(self.a + other)
 
+x = A(3)
+y = A(15)
+print(x, y)
 
-@prepend_dashes
-def observe(sightseen):
-    print('observe', sightseen)
+z = x + y
+v = x + 'asdf'
 
-
-@prepend_dashes
-def eat(meal):
-    print('eat', meal)
-
-
-@prepend_dashes
-def rest():
-    print('rest')
-
-
-from functools import partial
-
-activities = [
-    partial(move_to, 'london'),
-    partial(observe, 'Thamse'),
-    partial(eat, 'tea'),
-    partial(rest),
-    partial(move_to, 'Paris'),
-    partial(observe, 'Eiffel'),
-    partial(eat, 'croasson'),
-    partial(rest ),
-    partial(move_to, 'home'),
-    partial(eat, 'whatever you like'),
-    partial(observe, 'tv'),
-    partial(rest),
-    partial(observe, 'tv'),
-    partial(rest)
-]
-
-for activity in activities:
-    activity()
+print(z, v)
