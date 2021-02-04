0d8d41d2abf74e53a6673edad99a9e27fb3e98c8
Alexey Ruzin <ruzin@kokoc.com>
2021-02-04 22:12:05 +0300

    __iadd__

--- lesson_7/classwork.py
+++ lesson_7/classwork.py
@@ -1,11 +1,21 @@
 
-class A:
-    def __init__(self, xxx):
-        self.xxx = xxx
+class Wheel:
+    def __init__(self, radius, width):
+        self.radius = radius
+        self.width = width
 
-    def __getitem__(self, index):
-        return self.xxx[-index]
+    def __iadd__(self, other):
+        if self.radius == other.radius:
+            self.width += other.width
+        else:
+            raise Exception('Два колеса разных радиусов нельзя скрепить')
+        return self
 
 
-x = A('asdfasdf')
-print(x[2])
+
+x = Wheel(12, 200)
+y = Wheel(12, 100)
+
+x += y
+
+print(x.width)
